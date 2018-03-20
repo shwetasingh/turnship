686a65d1674c885c0fd527e5a28c5f3a61ffc279
Singh, Shweta <shweta_singh@intuit.com>
2018-03-19 22:11:11 -0700

    updated color scheme. Replaced bg images logo etc.

--- js/custom.js
+++ js/custom.js
@@ -14,42 +14,6 @@
         }, 300);
     }).trigger("resize");
 
-
-    // 2.0 Welcome Slider active code
-
-    if ($.fn.owlCarousel) {
-        $(".welcome_slides").owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: true,
-            nav: true,
-            navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
-            dots: false,
-            autoplay: true,
-            autoplayTimeout: 7000,
-            smartSpeed: 500,
-            autoplayHoverPause: false
-        });
-    }
-
-    var owl = $('.welcome_slides');
-    owl.owlCarousel();
-    owl.on('translate.owl.carousel', function (event) {
-        $('.owl-item .single_slide .slide_text h2').removeClass('animated').hide();
-        $('.owl-item .single_slide .slide_text h3').removeClass('animated').hide();
-        $('.owl-item .single_slide .slide_text .btn.btn-1').removeClass('animated').hide();
-        $('.owl-item .single_slide .slide_text .btn.btn-2').removeClass('animated').hide();
-    })
-
-    owl.on('translated.owl.carousel', function (event) {
-        $('.owl-item.active .single_slide .slide_text h2').addClass('animated custom_slideInUp').show();
-        $('.owl-item.active .single_slide .slide_text h3').addClass('animated custom_slideInUp_2').show();
-        $('.owl-item.active .single_slide .slide_text .btn.btn-1').addClass('animated custom_slideInUp_btn_1').show();
-        $('.owl-item.active .single_slide .slide_text .btn.btn-2').addClass('animated custom_slideInUp_btn_2').show();
-    })
-
-
-
     // 2.0 search box active code
     var mainMenu = $(".main_menu_area .search_button");
     mainMenu.on('click', function () {
